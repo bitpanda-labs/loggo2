40aa76c4187ec6965eb9dae0a4428f9cff7df017
Danny <daniel.mcdonald@bitpanda.com>
2018-05-02 11:03:28 +0200

    figure out way to do config

--- loggo/loggo.py
+++ loggo/loggo.py
@@ -7,14 +7,32 @@ import os
 from datetime import datetime
 
 from .config import LOG_LEVELS as log_levels
-from .config import COLOUR_MAP
 
+# you don't need graylog installed, but it is really powerful
 try:
     import graypy
 except ImportError:
     graypy = None
 
+# colorama can be used to color your log messages, but also isn't needed
+try:
+    from colorama import init
+    init()
+    from colorama import Fore, Back, Style
+    COLOUR_MAP = dict(red=Fore.RED + Style.BRIGHT,
+                      green=Fore.GREEN + Style.BRIGHT,
+                      critical=Fore.WHITE + Back.RED + Style.BRIGHT,
+                      dev=Fore.RED + Style.BRIGHT,
+                      debug=Style.DIM,
+                      greendim=Style.DIM+Fore.GREEN,
+                      end=Style.RESET_ALL)
+except ImportError:
+    COLOUR_MAP = dict()
+
 def colour_msg(msg, alert):
+    """
+    Try to colour a message, if we can
+    """
     if alert is None:
         alert = COLOUR_MAP.get('greendim', '')
     else:
@@ -28,12 +46,11 @@ class Loggo(object):
 
     def __init__(self, facility='loggo', ip=None, port=None, do_print=True, do_write=False, **kwargs):
         """
-        Keyword arguments:
-
-          config (util.Config.Mixin()/None): a config to be passed in by tx2
+        Settomgs can be passed in from config.SETTINGS
         """
         from .decorator import logme
         from .exception import LoggedException
+        # copy all the args and kwargs into a dict for extra logging into
         self.log_data = locals().copy()
         self.facility = facility
         self.ip = ip
@@ -41,7 +58,9 @@ class Loggo(object):
         self.do_print = do_print
         self.do_write = do_write
         self.kwargs = kwargs
+        # this is a way to provide a loggable exception
         self.LoggedException = LoggedException
+        # the below is to be used as a decorator function
         self.logme = logme
 
         # build logger object and add graylog support if possible
@@ -54,13 +73,12 @@ class Loggo(object):
         Subclass and change this if you like
         """
         fpath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-        logpath = os.path.join(fpath, 'logs')
+        logpath = os.path.join('..', fpath, 'logs')
         if not os.path.exists(logpath):
             os.makedirs(logpath)
         return os.path.join(logpath, 'log.txt')
 
     def _build_string(self, msg, level, log_data):
-        log_data = str(log_data)
         tstamp = datetime.now().strftime('%d.%m %Y %H:%M:%S')
         datapoints = [tstamp, msg, level, log_data]
         return '\t'.join([str(s) for s in datapoints])
@@ -138,7 +156,41 @@ class Loggo(object):
             out[key] = value
         return out
 
+    def _parse_input(self, alert, log_data):
+        """
+        For compatibility reasons, the user can pass arguments to log in a many
+        different ways.
+
+        Returns:
+            log_data (dict): key-value pairs for graylog
+        """
+        if isinstance(alert, str) and alert.lower() == 'none':
+            alert = None
+        if not alert and not log_data:
+            return dict()
+        elif not alert and log_data:
+            if not isinstance(log_data, dict):
+                log_data = dict(log_data=str(log_data))
+            return log_data
+        elif alert and isinstance(log_data, dict):
+            log_data = dict(log_data)
+            log_data['alert'] = alert
+            return log_data
+        elif alert and log_data and not isinstance(log_data, dict):
+            return dict(log_data=log_data, alert=alert)
+        elif isinstance(alert, str) and not log_data:
+            return dict(alert=alert)
+        elif isinstance(alert, dict):
+            return alert
+        # if none of these worked, here's a fallback, but log it
+        meta_data = dict(alert=alert, log_data=log_data)
+        self.log('Issue parsing log input', 'dev', meta_data)
+        return log_data
+
     def sanitise(self, message, data=None):
+        """
+        Make data safe for logging
+        """
         message = self._force_string_and_truncate(message)
         # data needs to become a dict, ideally with something in it
         if data is None:
@@ -156,6 +208,7 @@ class Loggo(object):
         Main logging method. Takes message string, alert level, a dict
         """
         try:
+            data = self._parse_input(alert, data)
             message, string_data = self.sanitise(message, data)
             single_string = self._build_string(message, alert, string_data)
 
@@ -167,5 +220,25 @@ class Loggo(object):
 
             log_level = getattr(logging, log_levels.get(alert, 'INFO'))
             self.logger.log(log_level, message, extra=string_data)
+
         except Exception as error:
-            raise self.LoggedException(str(error), alert=alert, **data)
+            self._emergency_log('General log failure: ' + str(error), message, error)
+
+    def _emergency_log(self, error_msg, msg, exception):  #  no cover
+        """
+        If there is an exception during logging, log/print it
+        """
+        try:
+            if msg != error_msg:
+                self.log(error_msg, 'dev')
+                last_chance = getattr(exception, 'message', 'Unknown error in emergency log')
+                self.log(str(last_chance), 'dev')
+            else:
+                print(msg)
+                print('Exiting because the system is in infinite loop')
+                error_msg = str(getattr(exception, 'message', exception))
+                exit(999)
+        except Exception as error:
+            print('Emergency log exception... gl&hf')
+            print(str(error))
+            exit(999)
