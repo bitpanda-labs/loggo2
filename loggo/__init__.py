9cffc76f39f37be7de5a69280e3650bad4241982
Danny <daniel.mcdonald@bitpanda.com>
2018-09-26 15:23:05 +0200

    Bump version: 1.3.0 → 1.4.0

--- loggo/__init__.py
+++ loggo/__init__.py
@@ -4,4 +4,4 @@ Simpler namespace
 from .exception import LoggedException
 from .loggo import Loggo
 
-__version__ = '1.3.0'  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = '1.4.0'  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
