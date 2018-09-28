e111bae06132af915e071b0c1d3f65f24cb91fbc
Danny <daniel.mcdonald@bitpanda.com>
2018-09-28 13:19:07 +0200

    Bump version: 1.5.5 → 1.5.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='loggo',
-    version='1.5.5',  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+    version='1.5.6',  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
     author='Danny McDonald',
     author_email='daniel.mcdonald@bitpanda.com',
     description=('Python logging tools'),
