95710bdcaa9429a276b8aae7ed46c10a04cd9b06
Jon Wood <jon.wood@bitpanda.com>
2019-06-17 13:53:30 +0200

    back to separate test file

---
+++ tests/test_import.py
@@ -0,0 +1,25 @@
+import unittest
+import sys
+
+
+class TestWithoutGraypy(unittest.TestCase):
+    def setUp(self):
+        self._temp_graypy = None
+        if sys.modules.get("graypy"):
+            self._temp_graypy = sys.modules["graypy"]
+        sys.modules["graypy"] = None
+
+    def tearDown(self):
+        if self._temp_graypy:
+            sys.modules["graypy"] = self._temp_graypy
+        else:
+            del sys.modules["graypy"]
+
+    def tests_using_graypy(self):
+        from loggo.loggo import graypy
+
+        self.assertEqual(graypy, None)
+
+
+if __name__ == "__main__":
+    unittest.main()
