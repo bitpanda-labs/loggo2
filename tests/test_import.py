561083db48eb9a7a1eaab4e7d4952aaa700d4080
Jon Wood <jon.wood@bitpanda.com>
2019-06-17 16:08:57 +0200

    finally think it does it

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,25 +1,34 @@
 import unittest
 import builtins
+from typing import Mapping, Any
+from importlib import reload
+
+import_orig = builtins.__import__
+
+
+def mocked_import(name, *args):
+    if name == "graypy":
+        raise ImportError()
+    return import_orig(name, *args)
+
+
+builtins.__import__ = mocked_import
+
+test_setup = dict(
+    do_write=True, log_if_graylog_disabled=False, private_data={"mnemonic", "priv"}
+)  # type: Mapping[str, Any]
 
 
 class TestWithoutGraypy(unittest.TestCase):
-    def setUp(self):
-        self.import_orig = builtins.__import__
-
-        def mocked_import(name, *args):
-            if name == "graypy":
-                raise ImportError()
-            return self.import_orig(name, *args)
-
-        builtins.__import__ = mocked_import
-
     def tearDown(self):
-        builtins.__import__ = self.import_orig
+        builtins.__import__ = import_orig
 
     def tests_using_graypy(self):
-        from loggo.loggo import graypy
+        import loggo
 
-        self.assertEqual(graypy, None)
+        reload(loggo.loggo)
+        l = loggo.Loggo(**test_setup)
+        self.assertEqual(loggo.loggo.graypy, None)
 
 
 if __name__ == "__main__":
