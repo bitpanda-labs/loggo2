dcbdbd739bed63c4966cca99258a9af07e9ecc16
Jon Wood <jon.wood@bitpanda.com>
2019-06-17 15:22:34 +0200

    fake import

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,19 +1,20 @@
 import unittest
-import sys
+import builtins
 
 
 class TestWithoutGraypy(unittest.TestCase):
     def setUp(self):
-        self._temp_graypy = None
-        if sys.modules.get("graypy"):
-            self._temp_graypy = sys.modules["graypy"]
-        sys.modules["graypy"] = None
+        self.import_orig = builtins.__import__
+
+        def mocked_import(name, *args):
+            if name == "graypy":
+                raise ImportError()
+            return self.import_orig(name, *args)
+
+        builtins.__import__ = mocked_import
 
     def tearDown(self):
-        if self._temp_graypy:
-            sys.modules["graypy"] = self._temp_graypy
-        else:
-            del sys.modules["graypy"]
+        builtins.__import__ = self.import_orig
 
     def tests_using_graypy(self):
         from loggo.loggo import graypy
