8a3d74d3ec788ad5b695fbbecffdceae2da0cd7d
Jon Wood <jonwood123@hotmail.com>
2019-06-18 10:30:18 +0200

    Import coverage (#98)

---
+++ tests/test_import.py
@@ -0,0 +1,32 @@
+import unittest
+import builtins
+from importlib import reload
+
+
+class TestWithoutGraypy(unittest.TestCase):
+    def setUp(self):
+        self.import_orig = builtins.__import__
+
+        def mocked_import(name, *args):
+            if name == "graypy":
+                raise ImportError()
+            return self.import_orig(name, *args)
+
+        builtins.__import__ = mocked_import
+
+    def tearDown(self):
+        builtins.__import__ = self.import_orig
+        import loggo
+
+        reload(loggo.loggo)
+
+    def tests_using_graypy(self):
+        import loggo
+
+        reload(loggo.loggo)
+        loggo.Loggo()
+        self.assertEqual(loggo.loggo.graypy, None)
+
+
+if __name__ == "__main__":
+    unittest.main()
