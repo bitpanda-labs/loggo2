512476ffd24fcffc372402f542400b0d7eb33918
jan <jan.tofel@bitpanda.com>
2022-05-09 09:12:14 +0200

    Bump version: 10.1.0 → 10.1.1

--- loggo2/__init__.py
+++ loggo2/__init__.py
@@ -1,3 +1,3 @@
 from ._loggo2 import JsonLogFormatter, LocalLogFormatter, Loggo  # noqa: F401
 
-__version__ = "10.1.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "10.1.1"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
